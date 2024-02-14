f49052d741ce748e5ed433f8b5470adb023928cc
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-14 17:22:06 +0100

    Add files via upload

---
+++ Apuntes Javascript 1/script.js
@@ -0,0 +1,20 @@
+alert( 'Hola Mundo Desde El Peor Instituto De DAW De Zaragoza')
+let num1 = 11;
+let num2 = 12;
+
+let resultado = num1 + num2;
+alert("El Resultado De La Operación Es: " + resultado);
+
+const Nombre = "Felipe Ortiz";
+alert("El Nombre Del Estudiante Es: " + Nombre);
+
+const saludo = "Somos Los Más Duros";
+alert(saludo + " Del ITACA." );
+
+let edad = 18;
+
+if (edad >= 18){
+    console.log('Eres Mayor De Edad' + ' ' + edad);
+} else {
+    console.log('Eres Menor De Edad.');
+}
